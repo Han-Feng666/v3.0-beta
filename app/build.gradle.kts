765a094c2fecdeb844afa86b007a177fd483df0b
Han-Feng666 <monkeycode-ai@chaitin.com>
2026-05-24 02:52:11 +0000

    bump version to 3.1 beta Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.HanFeng"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "3.0-beta"
+        versionCode = 6
+        versionName = "3.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
